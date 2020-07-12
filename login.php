3333aca433d8f68f1c18eea55925b7a529be56cd
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-07-12 15:56:22 +0100

    Auth & session for login system

--- login.php
+++ login.php
@@ -1,20 +1,22 @@
-<?php
-include("dbcon.php");
 
-if(isset($_POST['login']))
-{
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-    $ret= mysqli_query("SELECT * FROM users WHERE username='$username' and password='$password'");
-    $num=mysqli_fetch_array($ret);
-    if($num>0)
-    {
-       echo "tobi";
-        header("location: lodge_data.php");
-    }
-    else
-    {
-        echo "incorrect username / password";
+<!-- <?php session_start(); ?>
+ --><?php 
+ include('dbcon.php'); 
+
+if (isset($_POST['login'])) {
+    $username = mysqli_real_escape_string($con, $_POST['username']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    $query = mysqli_query($con, "SELECT * FROM users WHERE  username='$username' and password='$password'");
+    $row = mysqli_fetch_array($query);
+    $num_row = mysqli_num_rows($query);
+
+    if ($num_row > 0) {
+        // $_SESSION['user_id'] = $row['user_id'];
+       
+            header("location: lodge_data.php");
+    } else {
+        echo 'Invalid Username and Password Combination';
     }
 }
 ?>
