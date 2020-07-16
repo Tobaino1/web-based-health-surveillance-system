d5714e7d6e484991b7cb37a3bf554f81e4a3e82c
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-07-16 23:35:57 +0100

    dbcon modification for server

--- dbcon.php
+++ dbcon.php
@@ -7,3 +7,12 @@ if (mysqli_connect_errno())
 
 }
 ?>     
+
+<!-- ?3gU66KhbiMwMq#X   = password to db on server -->
+
+<!-- dbname =   id8481695_health_surveillance_system -->
+<!-- username db = id8481695_root -->
+<!-- db host = localhost -->
+
+<!-- $conn = mysqli_connect("localhost", "id8481695_root", "?3gU66KhbiMwMq#X", "id8481695_health_surveillance_system");
+ -->
